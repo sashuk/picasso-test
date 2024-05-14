ddfaffead78029edea684dad7315c7a078c3855f
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-05-14 13:34:58 +0200

    Cleanup before demo

--- cypress/support/component.ts
+++
@@ -1,9 +0,0 @@
-import 'happo-cypress'
-import '@cypress/skip-test/support'
-import 'cypress-plugin-tab'
-import 'cypress-real-events/support'
-import './commands.jsx'
-/* eslint-disable-next-line import/no-extraneous-dependencies */
-import '@toptal/davinci-qa/src/configs/cypress/support'
-// Import style file to support Tailwind CSS classes in Cypress tests.
-import '../assets/tailwind.css'

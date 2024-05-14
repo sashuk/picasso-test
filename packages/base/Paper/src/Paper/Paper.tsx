3ddc0c8836c1224ff28c4406958421badabb7a37
Aleksandr Shumilov <alexander.shumilov@toptal.com>
2024-05-14 16:29:58 +0200

    Add demo for cache problem

--- packages/base/Paper/src/Paper/Paper.tsx
+++ packages/base/Paper/src/Paper/Paper.tsx
@@ -48,7 +48,7 @@ export const Paper = forwardRef<HTMLDivElement, Props>(function Paper(
     <div
       ref={ref}
       className={twMerge(
-        'bg-white',
+        'bg-red-14',
         shadowsMapping[elevation],
         'transition-shadow duration-300 delay-0',
         className
--- packages/picasso/utils.js
+++
@@ -1,5 +0,0 @@
-// This file is here for the sake of compatability to allow TS versions below 5
-// to import from "@toptal/picasso/utils"
-// When possible, import directly from "@toptal/picasso-utils"
-export * from '@toptal/picasso-utils'
-export { useNotifications } from '@toptal/picasso-notification'
